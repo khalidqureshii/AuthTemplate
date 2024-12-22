d319f721472cfa5ca7e4019f5eca8873b749f28b
khalidqureshii <119107500+khalidqureshii@users.noreply.github.com>
2024-12-23 00:19:26 +0530

    Base Project

---
+++ client/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: ["class"],
+    content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+  	extend: {
+  		height: {
+  			'90vh': '90vh'
+  		},
+  		width: {
+  			'90vw': '90vw'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
+  },
+  plugins: [require("tailwindcss-animate")],
+}
+
